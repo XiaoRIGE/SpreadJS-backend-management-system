2a007099fa202f85b8091cece0c0376b3897676d
songrongjie <rongjie.song@1step.ai>
2023-10-25 12:39:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="spread-host">
+    <gc-spread-sheets hostClass="spreadHost" @workbookInitialized="initWorkbook">
+    </gc-spread-sheets>
+  </div>
+</template>
+
+<script setup>
+import "@grapecity/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css";
+import * as GC from "@grapecity/spread-sheets";
+function initWorkbook(spread) {
+  let sheet = spread.getActiveSheet();
+  sheet
+    .getCell(0, 0)
+    .vAlign(GC.Spread.Sheets.VerticalAlign.center)
+    .value("Hello SpreadJS");
+}
+</script>
+
+<style>
+.spreadHost {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
